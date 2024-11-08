d116ad0b8b9c8309731618b8e0709e5a360f139c
Kirill Biriukov <kirill.biriukov@cern.ch>
2024-11-08 14:19:19 +0100

    Changed the name of classes directory to validation and made __init__.py file. Now it is a package

---
+++ python/validation/DataFile.py
@@ -0,0 +1,58 @@
+import uproot as ur
+import awkward as ak
+import numpy as np
+
+# This class allows to open desired data of ticl_dumper.root files.
+# Methods starting from underscore are meant to be used ONLY inside other methods.
+#
+# Here the following convention is used:
+#       Each DataFile object consists of different "Branches". They have
+#       corresponding "branch_name"s.
+#       Each branch has a set of "key"s.
+#       CAUTION: Different branches may have different keys!
+#
+# Use getBranchKeys() if you want to know all the arrays inside a given branch.
+# Use openArray() to open desired array (awkward).
+
+class DataFile:
+    def __init__(self, filepath):
+        self.filepath = filepath
+        self.file = ur.open(filepath)
+        self.nevents = None
+        self.branches = self.file.keys()
+        self._nEvents()
+
+    def getBranchKeys(self, branch_name=''):
+        self._correctBranchName(branch_name)
+        return self.file[branch_name].keys()
+        
+    def openArray(self, branch_name='', key=''):
+        self._correctBranchName(branch_name)
+        self._correctKey(branch_name, key)
+
+        return self.file[branch_name][key].array()
+    
+    # Check if a given branch_name is available
+    def _correctBranchName(self, branch_name=''):
+        if branch_name not in self.branches:
+            raise NameError(f'branch name = {branch_name} is unavailable in data'\
+                            + f' file. \n Available branch names: \n {self.branches}')
+        return 0
+    
+    # Check if a given key is available for a given branch_name
+    def _correctKey(self, branch_name='', key=''):
+        if key not in self.getBranchKeys(branch_name):
+            keys = self.getBranchKeys(branch_name)
+            raise KeyError(f'key = {key} is unavailable in branch {branch_name}.'\
+                           + f'\n Available keys: \n {keys}')
+        return 0
+    
+    # Compute number of events in DataFile. Called automatically during
+    # initialization
+    def _nEvents(self):
+        if self.nevents is None:
+            n_events = len(self.openArray('ticlDumper/trackstersCLUE3DHigh;1',\
+                                       'NTracksters'))
+            self.nevents = n_events
+
+        return self.nevents
